9d630b319e1d5fbf53d576b88cba9b87e9a85f78
kofeychi <138976045+kofeychi@users.noreply.github.com>
2025-07-18 21:37:42 +0000

    JNI preparations idk

---
+++ src/jni/calls.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+use std::sync::{Mutex};
+use jni::JNIEnv;
+use jni::objects::{JObject, JValue, JValueOwned};
+use lazy_static::lazy_static;
+use crate::jni::jni::Modifier;
+#[derive(Clone)]
+pub struct Callable {
+    classpath: String,
+    method: String,
+    descriptor: String,
+    modifiers: Modifier
+}
+impl Callable {
+    pub unsafe fn new(id: String,path: String, method: String, descriptor: String, modifier: Modifier) -> Callable {
+        let mut callable = Callable{ classpath: path,method,descriptor, modifiers: modifier };
+        cache_add(id,callable.clone());
+        callable
+    }
+    pub fn call<'local>(self, mut jni: JNIEnv<'local>, args: &[JValue], obj: Option<JObject>) -> jni::errors::Result<JValueOwned<'local>> {
+        match self.modifiers.is_static() {
+            true => {
+                jni.call_static_method(self.classpath, self.method,self.descriptor,args)
+            },
+            false => {
+                jni.call_method(obj.unwrap(), self.method,self.descriptor,args)
+            }
+        }
+    }
+}
+pub unsafe fn cache_add(id: String,cal: Callable) {
+    CALLABLE_CACHE.lock().unwrap().insert(id,cal);
+}
+pub unsafe fn cache_get(cal: &String) {
+    CALLABLE_CACHE.lock().unwrap().get(cal);
+}
+lazy_static! {
+    static ref CALLABLE_CACHE: Mutex<HashMap<String,Callable>> = Mutex::new(HashMap::new());
+}
---
+++ src/jni/jni.rs
@@ -0,0 +1,120 @@
+#[derive(Clone)]
+pub struct JNIClass {
+    name: String,
+    modifiers: Modifier,
+
+    path: String,
+    fields: Vec<JNIField>,
+    methods: Vec<JNIMethod>,
+    constructors: Vec<JNIConstructor>,
+}
+#[derive(Clone)]
+pub struct Modifier {
+    modifiers: i32,
+}
+#[derive(Clone)]
+pub struct JNIMethod {
+    // commons
+    owner: JNIClass,
+    name: String,
+    modifiers: Modifier,
+    // commons
+    return_types: Vec<JNIClass>,
+    parameter_types: Vec<JNIClass>,
+    exception_types: Vec<JNIClass>
+}
+#[derive(Clone)]
+pub struct JNIField {
+    // commons
+    owner: JNIClass,
+    name: String,
+    modifiers: Modifier
+    // commons
+}
+#[derive(Clone)]
+pub struct JNIConstructor {
+    // commons
+    owner: JNIClass,
+    name: String,
+    modifiers: Modifier,
+    // commons
+    parameter_types: Vec<JNIClass>,
+    exception_types: Vec<JNIClass>
+}
+pub fn staticmodifier() -> Modifier {
+    Modifier {
+        modifiers: 8
+    }
+}
+
+impl Modifier {
+    /*
+    public static final int PUBLIC = 1;
+    public static final int PRIVATE = 2;
+    public static final int PROTECTED = 4;
+    public static final int STATIC = 8;
+    public static final int FINAL = 16;
+    public static final int SYNCHRONIZED = 32;
+    public static final int VOLATILE = 64;
+    public static final int TRANSIENT = 128;
+    public static final int NATIVE = 256;
+    public static final int INTERFACE = 512;
+    public static final int ABSTRACT = 1024;
+    public static final int STRICT = 2048;
+    public static final int SYNTHETIC = 4096;
+    public static final int ANNOTATION = 8192;
+    public static final int ENUM = 16384;
+    public static final int MANDATED = 32768;
+     */
+    fn mod_eq(&self,id: i32) -> bool {
+        (self.modifiers & id) != 0
+    }
+    pub fn is_public(&self) -> bool {
+        self.mod_eq(1)
+    }
+    pub fn is_private(&self) -> bool {
+        self.mod_eq(2)
+    }
+    pub fn is_protected(&self) -> bool {
+        self.mod_eq(4)
+    }
+    pub fn is_static(&self) -> bool {
+        self.mod_eq(8)
+    }
+    pub fn is_final(&self) -> bool {
+        self.mod_eq(16)
+    }
+    pub fn is_synchronized(&self) -> bool {
+        self.mod_eq(32)
+    }
+    pub fn is_volatile(&self) -> bool {
+        self.mod_eq(64)
+    }
+    pub fn is_transient(&self) -> bool {
+        self.mod_eq(128)
+    }
+    pub fn is_native(&self) -> bool {
+        self.mod_eq(256)
+    }
+    pub fn is_interface(&self) -> bool {
+        self.mod_eq(512)
+    }
+    pub fn is_abstract(&self) -> bool {
+        self.mod_eq(1024)
+    }
+    pub fn is_strict(&self) -> bool {
+        self.mod_eq(2048)
+    }
+    pub fn is_synthetic(&self) -> bool {
+        self.mod_eq(4096)
+    }
+    pub fn is_annotation(&self) -> bool {
+        self.mod_eq(8192)
+    }
+    pub fn is_enum(&self) -> bool {
+        self.mod_eq(16384)
+    }
+    pub fn is_mandated(&self) -> bool {
+        self.mod_eq(32768)
+    }
+}
